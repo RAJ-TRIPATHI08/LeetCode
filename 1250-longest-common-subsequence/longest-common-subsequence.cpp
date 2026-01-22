e8a6bc17d766e8b93ce2613d66c03c95fca52467
RAJ TRIPATHI <rajtripathi08@outlook.com>
2026-01-22 23:48:58 +0530

    Time: 31 ms (37.35%) | Memory: 13.1 MB (82.32%) - LeetSync

---
+++ 1250-longest-common-subsequence/longest-common-subsequence.cpp
@@ -0,0 +1,26 @@
+class Solution {
+private:
+    int dp[1001][1001];
+    int solve(string &s1, string &s2, int i, int j)
+    {
+        if(i >= s1.length() || j >= s2.length())
+            return 0;
+
+        if(dp[i][j] != -1) 
+            return dp[i][j];
+
+        if(s1[i] == s2[j])
+            return dp[i][j] = 1 + solve(s1, s2, i+1, j+1);
+        
+        return dp[i][j] = max(solve(s1, s2, i+1, j), solve(s1, s2, i, j+1));
+    }
+
+public:
+    int longestCommonSubsequence(string s1, string s2) {
+        int m = s1.size(), n = s2.size();
+
+        memset(dp, -1, sizeof(dp));
+
+        return solve(s1, s2, 0, 0);
+    }
+};
