eea8af4b512fe01552661d9d79ea3b2c0c4e000b
RAJ TRIPATHI <rajtripathi08@outlook.com>
2026-01-23 00:48:17 +0530

    Time: 47 ms (13.61%) | Memory: 13.5 MB (79.18%) - LeetSync

---
+++ 1437-minimum-insertion-steps-to-make-a-string-palindrome/minimum-insertion-steps-to-make-a-string-palindrome.cpp
@@ -0,0 +1,32 @@
+class Solution {
+private:
+    int dp[1001][1001];
+    int solve(string &s1, string &s2, int i, int j)
+    {
+        if(i >= s1.length() || j >= s2.length())
+            return 0;
+
+        if(dp[i][j] != -1) 
+            return dp[i][j];
+
+        if(s1[i] == s2[j])
+            return dp[i][j] = 1 + solve(s1, s2, i+1, j+1);
+        
+        return dp[i][j] = max(solve(s1, s2, i+1, j), solve(s1, s2, i, j+1));
+    }
+
+    int lcs(string s1, string s2) {
+        int m = s1.size(), n = s2.size();
+
+        memset(dp, -1, sizeof(dp));
+
+        return solve(s1, s2, 0, 0);
+    }
+
+public:
+    int minInsertions(string s) {
+        string s2 = s;
+        reverse(s2.begin(), s2.end());
+        return s.size() - lcs(s, s2);
+    }
+};
