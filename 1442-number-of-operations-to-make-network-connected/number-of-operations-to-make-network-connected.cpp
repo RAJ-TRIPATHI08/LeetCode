3d7be99c21c769594ae5d3f28498d82925092e6c
RAJ TRIPATHI <rajtripathi08@outlook.com>
2026-02-22 00:20:22 +0530

    Time: 67 ms (5.16%) | Memory: 63 MB (5.08%) - LeetSync

---
+++ 1442-number-of-operations-to-make-network-connected/number-of-operations-to-make-network-connected.cpp
@@ -0,0 +1,72 @@
+class Solution {
+public:
+    vector<int> par;
+    vector<int> rank;
+    
+    int find(int x)
+    {
+        
+        if(par[x] == x) return x;
+        return par[x] = find(par[x]);
+    }
+    
+    void unionSet(int u, int v)
+    {
+        int parU = find(u);
+        int parV = find(v);
+        
+        if(parU == parV) return;
+        
+        if(rank[parU] > rank[parV])
+        {
+            par[parV] = parU;
+        }
+        else if(rank[parU] < rank[parV])
+        {
+            par[parU] = parV;
+        }
+        else
+        {
+            par[parV] = parU;
+            rank[parU]++;
+        }
+    }
+
+    void dfs(unordered_map<int, vector<int>>& adj, int u, vector<bool>& vis)
+    {
+        vis[u] = true;
+
+        for(int v:adj[u])
+        {
+            if(!vis[v])
+            {
+                dfs(adj, v, vis);
+            }
+        }
+    }
+
+    int makeConnected(int n, vector<vector<int>>& connections) {
+        if(connections.size() < n-1) return -1;
+
+        unordered_map<int, vector<int>> adj(n);
+        vector<bool> vis(n, false);
+        
+        for(auto p:connections)
+        {
+            adj[p[0]].push_back(p[1]);
+            adj[p[1]].push_back(p[0]);
+        }
+
+        int cnt = 0;
+        for(int u = 0; u < n; u++)
+        {
+            if(!vis[u])
+            {
+                cnt++;
+                dfs(adj, u, vis);
+            }
+        }
+
+        return cnt - 1;
+    }
+};
