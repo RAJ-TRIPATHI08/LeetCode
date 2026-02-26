d884eb146a77eb3e4b2c9bf725ecab4cb65c6497
RAJ TRIPATHI <rajtripathi08@outlook.com>
2026-02-27 00:05:45 +0530

    Time: 8 ms (76.80%) | Memory: 51.2 MB (98.12%) - LeetSync

--- 820-find-eventual-safe-states/find-eventual-safe-states.cpp
+++ 820-find-eventual-safe-states/find-eventual-safe-states.cpp
@@ -1,7 +1,25 @@
 class Solution {
 public:
+    bool isCycleBFS(vector<vector<int>>& graph, int u, vector<bool>& vis, vector<bool>&inRec)
+    {
+        vis[u] = true;
+        inRec[u] = true;
+
+        for(int v:graph[u])
+        {
+            if(!vis[v] && isCycleBFS(graph, v, vis, inRec))
+                return true;
+            else if(inRec[v] == true)
+                return true;
+        }
+
+        inRec[u] = false;
+        return false;
+    }
+
     vector<int> eventualSafeNodes(vector<vector<int>>& graph) {
         int V = graph.size();
+        /* BFS approach using Kahn's algo
         vector<vector<int>> revAdj(V);
         vector<int> indegree(V, 0);
 
@@ -36,8 +54,22 @@ public:
                     q.push(v);
             }
         }
+        sort(safeNodes.begin(), safeNodes.end()); */
 
-        sort(safeNodes.begin(), safeNodes.end());
+        vector<bool> vis(V, false);
+        vector<bool> inRec(V, false);
+
+        for(int u = 0; u < V; u++)
+        {
+            if(!vis[u])
+                isCycleBFS(graph, u, vis, inRec);
+        }
+
+        vector<int> safeNodes;
+        for(int u = 0; u < V; u++)
+            if(!inRec[u]) safeNodes.push_back(u);
+            
+
         return safeNodes;
     }
 };
