f09a501ebfe42ac72110ebf7cbc57e026a5ea5f8
RAJ TRIPATHI <rajtripathi08@outlook.com>
2026-03-07 23:45:59 +0530

    Time: 0 ms (100.00%) | Memory: 9.1 MB (80.82%) - LeetSync

---
+++ 8-string-to-integer-atoi/string-to-integer-atoi.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    int myAtoi(string s) {
+        int i = 0, n = s.size();
+        
+        // 1. Skip leading spaces
+        while(i < n && s[i] == ' ')
+            i++;
+        
+        // 2. Sign
+        int sign = 1;
+        if(i < n && (s[i] == '+' || s[i] == '-')) {
+            if(s[i] == '-') sign = -1;
+            i++;
+        }
+        
+        long num = 0;
+        
+        // 3. Convert digits
+        while(i < n && isdigit(s[i])) {
+            num = num * 10 + (s[i] - '0');
+            
+            // 4. Handle overflow
+            if(sign * num >= INT_MAX)
+                return INT_MAX;
+            if(sign * num <= INT_MIN)
+                return INT_MIN;
+            
+            i++;
+        }
+        
+        return sign * num;
+    }
+};
