e15769e3714e28274540fa20097145a8d3e124c0
RAJ TRIPATHI <rajtripathi08@outlook.com>
2026-03-11 11:42:58 +0530

    Time: 0 ms (100.00%) | Memory: 8.1 MB (7.36%) - LeetSync

---
+++ 476-number-complement/number-complement.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+string removeLeadingZeros(string s) {
+        int pos = s.find('1'); // first index of '1'
+
+        if (pos == string::npos) // if no '1' exists
+            return "0";
+
+        return s.substr(pos);
+    }
+    int findComplement(int num) {
+        string s = bitset<64>(num).to_string();
+        s = removeLeadingZeros(s);
+        for (int i = 0; i < s.length(); i++) {
+            s[i] == '0' ? s[i] = '1' : s[i] = '0';
+        }
+
+        int res = stoi(s, nullptr, 2);
+        return res;
+    }
+};
