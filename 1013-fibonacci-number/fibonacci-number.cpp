fb3236cb0b228310c599103f6c8cf8c871e97a0c
RAJ TRIPATHI <rajtripathi08@outlook.com>
2026-03-25 01:00:10 +0530

    Time: 0 ms (100.00%) | Memory: 7.8 MB (64.38%) - LeetSync

--- 1013-fibonacci-number/fibonacci-number.cpp
+++ 1013-fibonacci-number/fibonacci-number.cpp
@@ -1,17 +1,19 @@
 class Solution {
 public:
-    vector<int> dp;
-
-    int fibo(int n)
-    {
-        if(n == 0 || n == 1) return n;
-        if(dp[n] != -1) return dp[n];
-
-        return dp[n] = fibo(n-1) + fibo(n-2);
-    }
-    
     int fib(int n) {
-        dp.resize(n+1, -1);
-        return fibo(n);
+        if(n == 0 || n == 1)  
+            return n;
+
+        int dp[n+1];
+
+        for(int i = 0; i <= n; i++)
+            dp[i] = 0;
+
+        dp[1] = 1;
+        for(int i = 2; i <= n; i++)
+        {
+            dp[i] = dp[i-1] + dp[i-2];
+        }
+        return dp[n];
     }
 };
