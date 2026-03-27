7ccd8518670bf5cd03b52008f8206ce90062a8e1
RAJ TRIPATHI <rajtripathi08@outlook.com>
2026-03-27 11:52:17 +0530

    Time: 859 ms (55.36%) | Memory: 389.2 MB (51.19%) - LeetSync

--- 3850-equal-sum-grid-partition-ii/equal-sum-grid-partition-ii.cpp
+++ 3850-equal-sum-grid-partition-ii/equal-sum-grid-partition-ii.cpp
@@ -7,50 +7,6 @@ public:
     // // to encounter all possible ways we are reversing transposing so we can check 
     // // vertical cases also
 
-
-    // bool checkCuts(vector<vector<int>>& grid)
-    // {
-    //     int n = grid.size();
-    //     int m = grid[0].size();
-    //     ll top = 0;
-    //     unordered_set<ll> st;
-
-    //     for(int i = 0; i <= n - 2; i++)
-    //     {
-    //         for(int j = 0; j < m; j++)
-    //         {
-    //             st.insert(grid[i][j]);
-    //             top += grid[i][j];
-    //         }
-
-    //         ll bottom = total - top;
-    //         ll diff = top - bottom;
-
-    //         if(diff == 0)   
-    //             return true;
-
-    //         // check for connectivities
-
-    //         // Case1: if it is present at top corner left
-    //         if(diff == (ll)grid[0][0])
-    //             return true;
-
-    //         // Case2: if it is present at top corner right
-    //         if(diff == (ll)grid[0][m-1])
-    //             return true;
-
-    //         // if only one column
-    //         // Case3: if it is present at just above the cutline 
-    //         if(diff == (ll)grid[i][0])
-    //             return true;
-
-    //         // Case4: if there is multiple rows then there is no issue of connectivity
-    //         if(i > 0 && m > 1 && st.count(diff))
-    //             return true;
-    //     }
-    //     return false;
-    // }
-
     bool canPartitionGrid(vector<vector<int>>& grid) {
         int n = grid.size();
         int m = grid[0].size();
