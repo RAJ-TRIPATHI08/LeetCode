8343b53734b40dcfcf009fe03a72acec4041f6ae
RAJ TRIPATHI <rajtripathi08@outlook.com>
2026-04-22 15:41:41 +0530

    Time: 19 ms (29.41%) | Memory: 19.1 MB (30.48%) - LeetSync

--- 2550-words-within-two-edits-of-dictionary/words-within-two-edits-of-dictionary.cpp
+++ 2550-words-within-two-edits-of-dictionary/words-within-two-edits-of-dictionary.cpp
@@ -1,34 +1,29 @@
 class Solution {
 public:
-    vector<string> twoEditWords(vector<string>& queries, vector<string>& dictionary) {
+    vector<string> twoEditWords(vector<string>& queries,
+                                vector<string>& dictionary) {
         vector<string> res;
 
         int n = queries.size();
         int m = dictionary.size();
-        
-        for(string s : queries)
-        {
-            for(string t : dictionary)
-            {
-                if(s == t)
-                {
+
+        for (string s : queries) {
+            for (string t : dictionary) {
+                if (s == t) {
                     res.push_back(s);
                     cout << s << endl;
                     break;
-                }
-                else
-                {
+                } else {
                     int l = s.length();
                     int missMatch = 0;
 
-                    for(int k = 0; k < l; k++)
-                    {
-                        if(s[k] == t[k])
+                    for (int k = 0; k < l; k++) {
+                        if (s[k] == t[k])
                             continue;
                         else
                             missMatch++;
                     }
-                    if(missMatch <= 2){
+                    if (missMatch <= 2) {
                         res.push_back(s);
                         break;
                     }
