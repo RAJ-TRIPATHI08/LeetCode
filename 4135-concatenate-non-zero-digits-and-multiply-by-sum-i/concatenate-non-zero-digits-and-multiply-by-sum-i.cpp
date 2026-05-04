5f53073c74c3da3f6ae30f3d298943f04f61a16d
RAJ TRIPATHI <rajtripathi08@outlook.com>
2026-05-04 20:16:27 +0530

    Time: 0 ms (100.00%) | Memory: 8.8 MB (98.66%) - LeetSync

--- 4135-concatenate-non-zero-digits-and-multiply-by-sum-i/concatenate-non-zero-digits-and-multiply-by-sum-i.cpp
+++ 4135-concatenate-non-zero-digits-and-multiply-by-sum-i/concatenate-non-zero-digits-and-multiply-by-sum-i.cpp
@@ -1,21 +1,39 @@
 class Solution {
 public:
     typedef long long ll;
-    ll sumAndMultiply(int n) {
-        string N = to_string(n);
-
-        ll x = 0;
-        ll sum = 0;
-        for(char c : N)
-        {
-            if(c == '0')
-                continue;
-            
-            int val = c - '0';
-            x = x * 10 + val;
-            sum += val;
+    int rev(int n) {
+        int reverse = 0;
+        while(n > 0) {
+            int digit = n % 10;
+            if(digit != 0) reverse = reverse * 10 + digit;
+            n = n / 10;
         }
+        return reverse;
+    }
 
-        return x*sum;
+    int rev1(int n) {
+        int reverse = 0;
+        while(n > 0) {
+            int digit = n % 10;
+            reverse = reverse * 10 + digit;
+            n = n / 10;
+        }
+        return reverse;
+    }
+
+    ll sum(int n) {
+        ll ans = 0;
+        while(n > 0) {
+            ans += n % 10;
+            n = n / 10;
+        }
+        return ans;
+    }
+    
+    long long sumAndMultiply(int n) {
+        ll num = rev(n);
+        num = rev1(num);
+        ll ans = num * sum(num);
+        return ans;
     }
 };
