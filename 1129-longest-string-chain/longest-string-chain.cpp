3abbe0c3bd67303090e2cb721f27580e49460141
RAJ TRIPATHI <rajtripathi08@outlook.com>
2026-06-06 14:59:43 +0530

    Time: 48 ms (53.71%) | Memory: 18.7 MB (72.55%) - LeetSync

--- 1129-longest-string-chain/longest-string-chain.cpp
+++ 1129-longest-string-chain/longest-string-chain.cpp
@@ -1,6 +1,4 @@
 class Solution {
-public:
-    int dp[1001][1001];
     bool isPredecessor(string &a, string &b) {
         if (b.size() != a.size() + 1)
             return false;
@@ -19,31 +17,25 @@ public:
         return i == a.size();
     }
 
-    int solve(vector<string>& a, int idx, int prev)
-    {
-        if(idx >= a.size())
-            return 0;
-
-        if(dp[prev+1][idx] != -1)
-            return dp[prev+1][idx];
-
-        int pick = 0;
-
-        if(prev == -1 || isPredecessor(a[prev], a[idx]))
-            pick = 1 + solve(a, idx + 1, idx);
-
-        int notPick = solve(a, idx + 1, prev);
-
-        return dp[prev+1][idx] = max(pick, notPick);
-    }
-
+public:
     int longestStrChain(vector<string>& words) {
-        memset(dp, -1, sizeof(dp));
+        int n = words.size();
         sort(words.begin(), words.end(),
         [](string &x, string &y) {
             return x.size() < y.size();
         });
 
-        return solve(words, 0, -1);
+        vector<int> dp(n, 1);
+
+        for(int i = n - 1; i >= 0; i--) 
+        {
+            for(int j = i + 1; j < n; j++) 
+            {
+                if(isPredecessor(words[i], words[j])) 
+                    dp[i] = max(dp[i], 1 + dp[j]);
+            }
+        }
+
+        return *max_element(dp.begin(), dp.end());
     }
 };
