1bf390cdf6e8ba71b4a36fd32730be72544d888f
RAJ TRIPATHI <rajtripathi08@outlook.com>
2026-06-12 12:25:31 +0530

    Time: 644 ms (12.50%) | Memory: 429.3 MB (5.92%) - LeetSync

--- 3844-number-of-ways-to-assign-edge-weights-i/number-of-ways-to-assign-edge-weights-i.cpp
+++ 3844-number-of-ways-to-assign-edge-weights-i/number-of-ways-to-assign-edge-weights-i.cpp
@@ -1,33 +1,51 @@
 class Solution {
-public:
-    const int MOD=1e9+7;
-int power(long long a,long long b){
-    long long ans=1;
-        a%=MOD;
-     while(b>0){
-        if(b&1)ans=(ans*a)%MOD;
-        a=(a*a)%MOD;
-        b>>=1;
+    using ll = long long;
+
+    ll MOD = 1e9 + 7;
+
+    ll binPow(ll a, ll b) 
+    {
+        if (b == 0) return 1;
+
+        ll half = binPow(a, b / 2);
+        ll res = (half * half) % MOD;
+
+        if (b & 1)
+            res = (res * (a % MOD)) % MOD;
+
+        return res;
     }
-    return (int)ans;
-}
-    int dfs(int node,int per,vector<vector<int>>&adj){
-        if(adj[node].size()==0)return 0;
-        int ans=0;
-        for(auto&it:adj[node]){
-            if(it!=per)ans=max(ans,1+dfs(it,node,adj));
+
+    int getMaxDepth(unordered_map<int, vector<int>>& adj, int node, int par)
+    {
+        int maxDepth = 0;
+
+        for(auto nbr : adj[node])
+        {   
+            if(nbr == par) 
+                continue;
+
+            maxDepth = max(maxDepth, getMaxDepth(adj, nbr, node)+1);
         }
-        return ans;
+
+        return maxDepth;
     }
+
+public:
     int assignEdgeWeights(vector<vector<int>>& edges) {
-        int n=edges.size()+2;
-        vector<vector<int>>adj(n);
-        for(auto&edge : edges){
-            int u=edge[0],v=edge[1];
+        int n = edges.size();
+        unordered_map<int, vector<int>> adj;
+
+        for(auto e : edges)
+        {
+            int u = e[0];
+            int v = e[1];
+        
             adj[u].push_back(v);
             adj[v].push_back(u);
         }
-        int depth=dfs(1,-1,adj);
-        return power(2,depth-1);
+
+        int d = getMaxDepth(adj, 1, -1);
+        return binPow(2, d-1);
     }
 };
