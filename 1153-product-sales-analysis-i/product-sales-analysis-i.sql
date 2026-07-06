07bf55a83d0cca99919eedb9b0b052dea10c1735
RAJ TRIPATHI <rajtripathi08@outlook.com>
2026-07-06 13:03:25 +0530

    Time: 1431 ms (22.86%) | Memory: 0B (100.00%) - LeetSync

--- 1153-product-sales-analysis-i/product-sales-analysis-i.sql
+++ 1153-product-sales-analysis-i/product-sales-analysis-i.sql
@@ -2,8 +2,8 @@
 SELECT 
     p.product_name, s.year, s.price
 FROM 
-    Sales s
-JOIN 
     Product p
+JOIN 
+    Sales s                                                          
 ON
     s.product_id = p.product_id
