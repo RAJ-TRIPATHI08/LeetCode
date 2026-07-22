641d571822452e415503877d408d39a6061530d4
RAJ TRIPATHI <rajtripathi08@outlook.com>
2026-07-22 10:44:30 +0530

    Time: 31 ms (59.38%) | Memory: 13.7 MB (97.25%) - LeetSync

--- 322-coin-change/coin-change.cpp
+++ 322-coin-change/coin-change.cpp
@@ -1,36 +1,34 @@
 class Solution {
 public:
-    int dp[12 + 1][10000 + 1];
-
-    int solve(vector<int> & coins, int curr, int amt)
+    int dp[13][10001];
+    int solve(vector<int>& a, int idx, int amt)
     {
-        if(curr >= coins.size() || amt <= 0)
-            return (amt == 0)? 0 : INT_MAX - 1;
+        if(idx >= a.size() || amt <= 0)
+            return (amt == 0)? 0:INT_MAX-1;
+
+        if(dp[idx][amt] != -1)
+            return dp[idx][amt];
 
         int res = -1;
-
-        if (dp[curr][amt] != -1)
-            return dp[curr][amt];
-
-        if(coins[curr] > amt)
+        if(a[idx] > amt)
         {
-            // not pick
-            int notPick = 0 + solve(coins, curr + 1, amt);
-            dp[curr][amt] = res = notPick;
+            int notPick = 0 + solve(a, idx+1, amt);
+            res = notPick;
+            dp[idx][amt] = res;
         }
         else
         {
-            // pick or not pick case
-            int pick = 1 + solve(coins, curr + 0, amt - coins[curr]);
-            int notPick = 0 + solve(coins, curr + 1, amt);
-            dp[curr][amt] = res = min(pick, notPick);
+            int pick = 1 + solve(a, idx, amt-a[idx]);
+            int notPick = 0 + solve(a, idx+1, amt);
+            res = min(pick, notPick);
+            dp[idx][amt] = res;
         }
-        return dp[curr][amt] = res;
+        return dp[idx][amt] = res;
     }
 
     int coinChange(vector<int>& coins, int amount) {
         memset(dp, -1, sizeof(dp));
         int res = solve(coins, 0, amount);
-        return res == INT_MAX - 1? -1:res;
+        return (res == INT_MAX-1)? -1 : res;
     }
 };
