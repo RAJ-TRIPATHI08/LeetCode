d05c011f2c2064019ed9910262d3d6c708eff0e6
RAJ TRIPATHI <rajtripathi08@outlook.com>
2026-07-22 10:58:08 +0530

    Time: 13 ms (95.30%) | Memory: 18 MB (74.05%) - LeetSync

--- 322-coin-change/coin-change.cpp
+++ 322-coin-change/coin-change.cpp
@@ -1,34 +1,16 @@
 class Solution {
 public:
-    int dp[13][10001];
-    int solve(vector<int>& a, int idx, int amt)
-    {
-        if(idx >= a.size() || amt <= 0)
-            return (amt == 0)? 0:INT_MAX-1;
-
-        if(dp[idx][amt] != -1)
-            return dp[idx][amt];
-
-        int res = -1;
-        if(a[idx] > amt)
-        {
-            int notPick = 0 + solve(a, idx+1, amt);
-            res = notPick;
-            dp[idx][amt] = res;
-        }
-        else
-        {
-            int pick = 1 + solve(a, idx, amt-a[idx]);
-            int notPick = 0 + solve(a, idx+1, amt);
-            res = min(pick, notPick);
-            dp[idx][amt] = res;
-        }
-        return dp[idx][amt] = res;
-    }
-
     int coinChange(vector<int>& coins, int amount) {
-        memset(dp, -1, sizeof(dp));
-        int res = solve(coins, 0, amount);
-        return (res == INT_MAX-1)? -1 : res;
+        int n = coins.size();
+        vector<int> dp(amount+1, INT_MAX-1);
+        dp[0] = 0;
+        for(int c : coins)
+        {
+            for(int amt = c; amt <= amount; amt++)
+            {
+                dp[amt] = min(dp[amt], 1 + dp[amt-c]);
+            }
+        }
+        return dp[amount] == INT_MAX-1? -1 : dp[amount];
     }
 };
