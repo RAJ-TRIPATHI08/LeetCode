939e218ba6b03663033d8fbd59f01a6414115f15
RAJ TRIPATHI <rajtripathi08@outlook.com>
2026-07-22 10:58:38 +0530

    Time: 15 ms (92.70%) | Memory: 17.8 MB (94.47%) - LeetSync

--- 322-coin-change/coin-change.cpp
+++ 322-coin-change/coin-change.cpp
@@ -5,12 +5,9 @@ public:
         vector<int> dp(amount+1, INT_MAX-1);
         dp[0] = 0;
         for(int c : coins)
-        {
             for(int amt = c; amt <= amount; amt++)
-            {
                 dp[amt] = min(dp[amt], 1 + dp[amt-c]);
-            }
-        }
+
         return dp[amount] == INT_MAX-1? -1 : dp[amount];
     }
 };
