bf9ef105b5dfd8ae016228d39007100883ff277f
RAJ TRIPATHI <rajtripathi08@outlook.com>
2026-09-17 22:26:20 +0530

    Time: 3 ms (94.59%) | Memory: 88.8 MB (79.38%) - LeetSync

--- 1573-find-two-non-overlapping-sub-arrays-each-with-target-sum/find-two-non-overlapping-sub-arrays-each-with-target-sum.cpp
+++ 1573-find-two-non-overlapping-sub-arrays-each-with-target-sum/find-two-non-overlapping-sub-arrays-each-with-target-sum.cpp
@@ -5,16 +5,16 @@ public:
         int i = 0;
         int j = 0;
 
-        int minLen = INT_MAX, minLen1 = INT_MAX;
         int res = INT_MAX;
         int bestLen = INT_MAX;
         int sum = 0;
+
         vector<int> dp(n, INT_MAX);
         while(j < n)
         {
             sum += arr[j];
 
-            while(sum > target && i < j)
+            while(sum > target && i <= j)
             {
                 sum -= arr[i];
                 i++;
@@ -33,9 +33,8 @@ public:
             j++;
         }
 
-        if(res == INT_MAX)
-            return -1;
+        
 
-        return res;
+        return res != INT_MAX? res : -1;
     }
 };
